aac33c9ebf2a7c9a0c230c6de960b1074b4a090b
TheLore85 <garofalo.digital@gmail.com>
2024-07-13 15:01:26 +0200

    shadcs running locally

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import {useState} from 'react'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faBell} from '@fortawesome/free-solid-svg-icons'
 import './App.css'
+import {Button} from './components/ui/button'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
         <FontAwesomeIcon icon={faBell} />
       </div>
       <h1 className="text-pink-400">Vite + React</h1>
+      <Button>Call me now</Button>
       <div className="card">
         <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
       </div>
